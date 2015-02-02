392df84dfb6313ec50462cbc871c1896d604cc59
Carlos Beltran <cbeltr4@gmail.com>
2015-02-02 16:41:32 -0600

    Fix store layer, finish adding ui elements and art

---
+++ TappingGame/Meter.swift
@@ -0,0 +1,9 @@
+//
+//  Meter.swift
+//  TappingGame
+//
+//  Created by Carlos Beltran on 1/30/15.
+//  Copyright (c) 2015 Carlos. All rights reserved.
+//
+
+import Foundation
---
+++ TappingGame/StoreLayer.swift
@@ -0,0 +1,9 @@
+//
+//  StoreLayer.swift
+//  TappingGame
+//
+//  Created by Carlos Beltran on 2/1/15.
+//  Copyright (c) 2015 Carlos. All rights reserved.
+//
+
+import Foundation
